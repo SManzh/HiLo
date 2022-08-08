97f437d8e991679cb95aeb122db4ad96124ebb36
Vyacheslav Manzhela <slavamanzh@gmail.com>
2022-08-08 21:38:12 +0300

    feat: start writing code

---
+++ src/HiLo.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class HiLo {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String playAgain = "";
+		do {
+			//Random number generation
+		}
+
+	}
+
+}
